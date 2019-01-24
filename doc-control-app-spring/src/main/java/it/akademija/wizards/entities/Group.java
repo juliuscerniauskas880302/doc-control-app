e1748b04ad3b9b6d5c596b128c154f213225fe9a
Vytautas Mickevicius <mickev.vytautas@gmail.com>
2019-01-24 11:34:47 +0200

    Entities User, Documents and Group are added

--- doc-control-app-spring/src/main/java/it/akademija/wizards/entities/Group.java
+++ doc-control-app-spring/src/main/java/it/akademija/wizards/entities/Group.java
@@ -1,12 +1,17 @@
 package it.akademija.wizards.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Group {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    public Group() {
+    }
 }
