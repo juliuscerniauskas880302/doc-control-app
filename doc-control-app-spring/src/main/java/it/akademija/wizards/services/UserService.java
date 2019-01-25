9c3a18e658caa7823125d64be195c8b47e43987e
Andrius <olisauskas.andrius@gmail.com>
2019-01-25 12:58:28 +0200

    Imports fixed

--- doc-control-app-spring/src/main/java/it/akademija/wizards/services/UserService.java
+++ doc-control-app-spring/src/main/java/it/akademija/wizards/services/UserService.java
@@ -1,5 +1,6 @@
 package it.akademija.wizards.services;
 
+import it.akademija.wizards.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
