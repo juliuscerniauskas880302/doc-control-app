ef1db279c0112b73c0881f1e50fae77b749f52b2
Vytautas <mickev.vytautas@gmail.com>
2019-02-04 14:33:16 +0200

    Additional files archiving, swagger-UI corrupting zip files not resolved

--- doc-control-app-spring/src/main/java/it/akademija/wizards/controllers/DocumentController.java
+++ doc-control-app-spring/src/main/java/it/akademija/wizards/controllers/DocumentController.java
@@ -18,17 +18,16 @@ import org.springframework.context.ApplicationContext;
 
 
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 
+import java.nio.file.Files;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -117,21 +116,24 @@ public class DocumentController {
         }
         return ResponseEntity.notFound().build();
     }
-
-
+    //TODO: solve ZIP corruption problem in swagger-UI
     @ApiOperation(value = "download additionalFiles")
     @RequestMapping(value = "/{id}/download/attachments", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<byte[]> downloadAttachments(@PathVariable final String id) throws IOException {
-
+    public ResponseEntity<InputStreamResource> downloadAttachments(
+            @PathVariable final String id) throws IOException {
         DocumentGetCommand document = documentService.getDocumentsById(id);
         List<String> fileNames = document.getAdditionalFilePaths();
         if (fileNames != null) {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ZipOutputStream zos = new ZipOutputStream(baos);
-            byte bytes[] = new byte[2048];
-
+            //            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            FileOutputStream fos = new FileOutputStream(
+                    "documents" + File.separator
+                            + document.getAuthor().getUsername()
+                            + File.separator + "compressed.zip");
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            byte[] bytes = new byte[2048];
             for (String fileName : fileNames) {
+                String originalFileName = fileName.replace(document.getPrefix(), "");
                 FileInputStream fis = new FileInputStream(
                         "documents"
                                 + File.separator
@@ -139,7 +141,7 @@ public class DocumentController {
                                 + File.separator
                                 + fileName);
                 BufferedInputStream bis = new BufferedInputStream(fis);
-                zos.putNextEntry(new ZipEntry(fileName));
+                zos.putNextEntry(new ZipEntry(originalFileName));
                 int bytesRead;
                 while ((bytesRead = bis.read(bytes)) != -1) {
                     zos.write(bytes, 0, bytesRead);
@@ -149,26 +151,76 @@ public class DocumentController {
                 fis.close();
             }
             zos.flush();
-            baos.flush();
+            //            baos.flush();
+            fos.flush();
             zos.close();
-            baos.close();
-
+            //            baos.close();
+            fos.close();
+            //            InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(baos.toByteArray()));
+            InputStreamResource resource = new InputStreamResource(
+                    new FileInputStream(
+                            new File("documents" + File.separator
+                                    + document.getAuthor().getUsername()
+                                    + File.separator + "compressed.zip")));
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"zipFile.zip\"");
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"compressed.zip\"");
             headers.add("Access-Control-Expose-Headers",
                     HttpHeaders.CONTENT_DISPOSITION);
-//            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.setContentType(MediaType.parseMediaType("application/zip"));
-
-            return ResponseEntity.ok().headers(headers).body(baos.toByteArray());
-
-        }
-        else{
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            return ResponseEntity.ok().headers(headers).body(resource);
+        } else {
             return ResponseEntity.notFound().build();
         }
 
     }
 
+
+//      DOWNLOAD WITH HTTPSERVLETRESPONSE
+//    @ApiOperation(value = "download additionalFiles")
+//    @RequestMapping(value = "/{id}/download/attachments", method = RequestMethod.GET,
+//    produces = "application/octet-stream")
+//    @ResponseStatus(HttpStatus.OK)
+//    public HttpEntity downloadAttachments(
+//            HttpServletResponse response, @PathVariable final String id) throws IOException {
+//
+//        File doc = new File("documents/migle" + "/" + "compressed.zip");
+//
+//        InputStream is = new FileInputStream(doc);
+//
+//        response.setHeader("Content-Disposition", "attachment;filename=\"compressed.zip\"");
+//        response.setHeader("Content-Type", "application/octet-stream;");
+//        StreamUtils.copy(is ,response.getOutputStream());
+//
+//        return new ResponseEntity(HttpStatus.OK);
+//        MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
+//        DocumentGetCommand document = documentService.getDocumentsById(id);
+//        List<String> fileNames = document.getAdditionalFilePaths();
+//        String fileName = "compressed.zip";
+//        System.out.println("fileName: " + fileName);
+//        System.out.println("mediaType: " + mediaType);
+//
+//        File file = new File("documents/migle" + "/" + fileName);
+//
+//        // Content-Type
+//        // application/pdf
+////        response.setContentType(mediaType.getType());
+//
+//        // Content-Disposition
+//        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName());
+//
+//        // Content-Length
+//        response.setContentLength((int) file.length());
+//
+//        BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(file));
+//        BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream());
+//
+//        byte[] buffer = new byte[1024];
+//        int bytesRead = 0;
+//        while ((bytesRead = inStream.read(buffer)) != -1) {
+//            outStream.write(buffer, 0, bytesRead);
+//        }
+//        outStream.flush();
+//        inStream.close();
 
     @ApiOperation(value = "submit document by document Id")
     @RequestMapping(value = "/{id}/submit", method = RequestMethod.PUT)
