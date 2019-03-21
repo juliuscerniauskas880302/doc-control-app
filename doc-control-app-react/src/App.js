188ac969c47eaa87c7532ef3e8ab77c9cc9cd128
Andrius <olisauskas.andrius@gmail.com>
2019-03-21 10:14:32 +0200

    Logout on error 401

--- doc-control-app-react/src/App.js
+++ doc-control-app-react/src/App.js
@@ -7,8 +7,20 @@ import "./css/style.default.css";
 import "./css/custom.css";
 import "popper.js";
 import "bootstrap";
+import Axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    Axios.interceptors.response.use(null, function (error) {
+      if (error.response.status === 401) {
+        localStorage.removeItem("accessToken");
+        delete Axios.defaults.headers.Authorization;
+        return <Route path="/" render={props => <Authentication {...props} />} />
+      }
+      return Promise.reject(error);
+    });
+  }
   render() {
     return (
       <Router>
